fa4edf5959ffb2d83722af85ad89143319e24e99
hoerul1224 <hoerul1224@gmail.com>
2026-07-17 15:09:15 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Koneksi ke MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB terhubung'))
+  .catch((err) => console.error('Gagal konek MongoDB:', err));
+
+app.get('/', (req, res) => {
+  res.send('Server backend berjalan!');
+});
+
+const cors = require('cors');
+// ...
+app.use(cors());
+
+const productRoutes = require('./routes/productRoutes');
+app.use('/api/products', productRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server jalan di http://localhost:${PORT}`);
+});
